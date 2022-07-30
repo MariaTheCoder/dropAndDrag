28e73badb25464a8b6152cf0a5926e7ef461e512
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-30 15:51:04 +0200

    Use e.dataTransfer.setData() to transfer the id from one element to the next. The second line hides the element item once we start dragging int.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Seleect the item element
+const item = document.querySelector(".item");
+
+// Attach the dragstart event handler
+item.addEventListener("dragstart", dragStart);
+
+// Handle the dragstart
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.id);
+  e.target.classList.add("hide");
+}
